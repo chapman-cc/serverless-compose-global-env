760773f99e4efa5563a2b35374881f29f77b6bc5
ChapmanCheng <chapboy1989@gmail.com>
2023-04-24 13:45:37 +0800

    test(serverlesslyaer.test.js): write test for ServerlessLayer.js

--- tests/ServerlessLayer.test.js
+++ tests/ServerlessLayer.test.js
@@ -1,34 +1,54 @@
-const { describe, before, after } = require('mocha');
-const expect = require('chai').expect;
-const fs = require('node:fs');
-const path = require('node:path');
+const { describe, before, after } = require("mocha");
+const expect = require("chai").expect;
+const fs = require("node:fs");
+const path = require("node:path");
+const ServerlessLayer = require("../lib/ServerlessLayer");
 
-const root = "/tmp/sls-layer-env"
+const root = "/tmp/sls-layer-env";
 
-const LEVELS = Object.freeze( {
-    one: "level-1",
-    two: "level-2",
-    three: "level-3",
+const LEVELS = Object.freeze({
+  one: "level-1",
+  two: "level-2",
+  three: "level-3",
 });
 
-before(function() {
-    fs.mkdirSync(path.join(root, LEVELS.one, LEVELS.two, LEVELS.three), { recursive: true });
+before(function () {
+  fs.mkdirSync(path.join(root, LEVELS.one, LEVELS.two, LEVELS.three), {
+    recursive: true,
+  });
 
-    fs.writeFileSync(path.join(root, LEVELS.one,  ".env"), "foobar=foo");
-    fs.writeFileSync(path.join(root, LEVELS.one, LEVELS.two, ".env"), "foobar=bar");
-    fs.writeFileSync(path.join(root, LEVELS.one, LEVELS.two, LEVELS.three, ".env"), "foobar=foobar");
+  fs.writeFileSync(path.join(root, LEVELS.one, ".env"), "foobar=foo");
+  fs.writeFileSync(
+    path.join(root, LEVELS.one, LEVELS.two, ".env"),
+    "foobar=bar"
+  );
+  fs.writeFileSync(
+    path.join(root, LEVELS.one, LEVELS.two, LEVELS.three, ".env"),
+    "foobar=foobar"
+  );
 
-    fs.writeFileSync(path.join(root, LEVELS.one,  ".env.dev"), "stage=dev")
-    fs.writeFileSync(path.join(root, LEVELS.one,  ".env.prod"), "stage=prod")
+  fs.writeFileSync(path.join(root, LEVELS.one, ".env.dev"), "stage=dev");
+  fs.writeFileSync(path.join(root, LEVELS.one, ".env.prod"), "stage=prod");
+});
 
-})
+after(function () {
+  fs.rmSync(root, { recursive: true });
+});
 
-after(function() {
-    fs.rmSync(root, { recursive: true });
-})
+describe("Serverless Layer", function () {
+  it("should load all env files", async function () {
 
-describe("Serverless Layer", function() {
-    it("should be able to run", function() {
-        expect(true).to.equal(true);
-    })
-})
+    // assume the lambda is nested in deepest level
+    const dir = path.resolve(root, LEVELS.one, LEVELS.two, LEVELS.three);
+    const layer = new ServerlessLayer(dir, "prod");
+    await layer.readEnvFiles();
+   
+    const env = layer.env;
+   
+    expect(env).to.haveOwnProperty("foobar");
+    expect(env).to.haveOwnProperty("stage");
+
+    expect(env.foobar).to.equal("foobar");
+    expect(env.stage).to.equal("prod");
+  });
+});
